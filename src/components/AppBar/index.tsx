59af54819828faaecb95b36b82b3380fb351679e
Victor Augusto <vaas@icomp.ufam.edu.br>
2021-05-18 21:49:48 -0400

    [Feat] Added routes config
    Signed-off-by: Victor Augusto <vaas@icomp.ufam.edu.br>

--- src/components/AppBar/index.tsx
+++ src/components/AppBar/index.tsx
@@ -1,19 +1,18 @@
+import Link from "next/link";
 import React from "react";
+import { Routes } from "../../config/routesConfig";
 
 const AppBar: React.FC = () => {
   return (
     <header className="bg-primary w-full p-5">
       <div className="container">
         <div className="flex w-full justify-end">
-          <a href="" className="text-black font-medium px-4">
-            ínicio
-          </a>
-          <a href="" className="text-black font-medium px-4">
-            artigos
-          </a>
-          <a href="" className="text-black font-medium px-4">
-            sobre
-          </a>
+          <Link href={Routes.HOME}>
+            <a className="text-black font-medium px-4">ínicio</a>
+          </Link>
+          <Link href={Routes.ABOUT}>
+            <a className="text-black font-medium px-4">sobre</a>
+          </Link>
         </div>
       </div>
     </header>
